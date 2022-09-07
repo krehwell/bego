664305b1fa0b6399f5f53a1175372545b1b280db
krehwell <fahrimuham001@gmail.com>
2022-09-07 13:13:48 +0700

    testing on select channel

--- 25Select.go
+++ 25Select.go
@@ -10,16 +10,20 @@ func ChannelSelect() {
     c2 := make(chan string)
 
     go func() {
-        time.Sleep(time.Second * 1)
-        c1<- "one"
+        for ; ; {
+            time.Sleep(time.Millisecond * 500)
+            c1<- "one"
+        }
     }()
 
     go func() {
-        time.Sleep(time.Second * 2)
-        c2<- "two"
+        for ; ; {
+            time.Sleep(time.Second * 2)
+            c2<- "two"
+        }
     }()
 
-    for i := 0; i < 2; i++ {
+    for ; ; {
         select {
         case msg1 := <-c1:
             fmt.Println("received", msg1)
